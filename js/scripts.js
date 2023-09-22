6ffdf7bf1e31813511a72be871e1849b8904ee1c
Moenul <moenul22@gmail.com>
2023-09-22 21:43:10 +0600

    Bootstrap Install and Modification Done

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,29 @@
 $(document).ready(function(){
 
-//  Scroll to top button -------------------
-    $(window).scroll(function() {
-        if ($(this).scrollTop()) {
-            $('.scroll_to_top').fadeIn();
-        } else {
-            $('.scroll_to_top').fadeOut();
-        }
-    });
+//  Nav Animate by Scroll and Scroll to top button -------------------
+$(window).scroll(function() {
+    if ($(this).scrollTop()) {
+        $(".nav_bar").addClass("nav_bar_active");
+    }
+    else {
+        $(".nav_bar").removeClass("nav_bar_active");
+    }
 
-    $(".scroll_to_top").click(function() {
-        $("html, body").animate({scrollTop: 0}, 500);
-     });
+    //  Scroll to top button -------------------
+    if ($(this).scrollTop()) {
+        $('.scroll_to_top').fadeIn();
+    } else {
+        $('.scroll_to_top').fadeOut();
+    }
+    //  Scroll to top button -------------------
+
+});
+//  Scroll to top button -------------------
+$(".scroll_to_top").click(function() {
+    $("html, body").animate({scrollTop: 0}, 300);
+});
+//  Scroll to top button -------------------
+//  Nav Animate by Scroll and Scroll to top button -------------------
 
 
     $(".nav_button").click(function() {
